c1df58fc20772f9913e55201194c443ca8d173ea
EC2 Default User <ec2-user@ip-10-1-0-29.ec2.internal>
2020-12-12 05:02:23 +0000

    test

---
+++ src/main/java/com/veeru1/org/App.java
@@ -0,0 +1,13 @@
+package com.veeru1.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
